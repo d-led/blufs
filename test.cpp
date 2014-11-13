1123a364a145354f24e6804292d3ff5b9385d298
Dmitry Ledentsov <dmlled@yahoo.com>
2014-11-13 21:53:37 +0100

    test structure

--- test.cpp
+++ test.cpp
@@ -15,17 +15,26 @@ public:
     LuaTest() :
         exec_path(unique_path())
     {
+        auto cpath =
+            std::string("package.cpath=[[")
+            + (current_path() / path("?.dll")).generic_string() + ";"
+            + (current_path() / path("?.so")).generic_string() + ";"
+            + "]] ..package.cpath";
+        state.doString(cpath);
+
         REQUIRE_NOTHROW(state.doString("require 'blufs'"));
+
         create_directories(exec_path);
         REQUIRE(exists(exec_path));
         current_path(exec_path);
     }
 
     ~LuaTest() {
+        current_path(current_path().parent_path());
         remove_all(exec_path);
     }
 };
 
-TEST_CASE_METHOD(LuaTest,"loading the library") {
+TEST_CASE_METHOD(LuaTest, "loading the library") {
     REQUIRE(state["blufs"].is<lua::Table>());
 }
