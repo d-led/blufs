52588a09b0d22a5f91a8f63796d19e75cba68825
Dmitry Ledentsov <dmlled@yahoo.com>
2014-11-13 21:31:01 +0100

    unique execution directory

--- test.cpp
+++ test.cpp
@@ -3,12 +3,26 @@
 
 #include <LuaState.h>
 
+#include <boost/filesystem.hpp>
+
+using namespace boost::filesystem;
+
 class LuaTest {
 protected:
     lua::State state;
+    path exec_path;
 public:
-    LuaTest() {
+    LuaTest() :
+        exec_path(unique_path())
+    {
         REQUIRE_NOTHROW(state.doString("require 'blufs'"));
+        create_directories(exec_path);
+        REQUIRE(exists(exec_path));
+        current_path(exec_path);
+    }
+
+    ~LuaTest() {
+        remove_all(exec_path);
     }
 };
 
