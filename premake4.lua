dd996781d9e4e646ce3e2055cb4a132a13c74fac
Dmitry Ledentsov <dmlled@yahoo.com>
2015-02-01 18:10:00 +0100

    windows build

--- premake4.lua
+++ premake4.lua
@@ -1,46 +1,28 @@
-_G.package.path=_G.package.path..[[;./?.lua;./?/?.lua]]
+include 'premake'
 
-assert( require 'premake.quickstart' )
+lua = assert(dofile 'premake/recipes/lua.lua')
+boost = assert(dofile 'premake/recipes/boost.lua')
 
 make_solution 'blufs'
 
-local OS = os.get()
-local settings = {
-	links = {
-		linux = { 'lua5.1', 'boost_system', 'boost_filesystem', 'dl', 'pthread' },
-		windows = { 'lua5.1' },
-		macosx = { 'lua', 'boost_system', 'boost_filesystem' }
-	}
-}
+lua:set_includedirs()
+lua:set_libdirs()
+boost:set_includedirs()
+boost:set_libdirs()
+boost:set_defines()
+defines { 'BOOST_NO_VARIADIC_TEMPLATES' }
 
 includedirs {
-	'./luabind',
-	'./Catch/single_include',
-	'./LuaState/include'
+	'./deps/luabind',
+	'./deps/Catch/single_include',
+	'./deps/LuaState/include'
 }
 
 local function platform_specifics()
 	make_cpp11()
-	-- platform specific --
-	configuration 'macosx'
-		targetprefix ''
-		includedirs {'/usr/local/include' }
-		libdirs { '/usr/local/lib' }
-		targetextension '.so'
-		targetdir ( './BuildClang' )		
-	configuration 'windows'
-		includedirs { [[C:\luarocks\2.1\include]] , os.getenv 'BOOST' }
-		libdirs { [[C:\luarocks\2.1]] , path.join(os.getenv'BOOST',[[lib32-msvc-12.0]]) }
-	configuration 'linux'
-		targetprefix ''
-		includedirs { [[/usr/include/lua5.1]] }
-		targetdir ( './Build' )
-	configuration { '*' }
 end
 
-defines { 'BOOST_NO_VARIADIC_TEMPLATES' }
-
-make_static_lib('luabind',{'./luabind/src/*.cpp'})
+make_static_lib('luabind',{'./deps/luabind/src/*.cpp'})
 platform_specifics()
 
 make_shared_lib('blufs', {
@@ -48,10 +30,13 @@ make_shared_lib('blufs', {
 	'blufs_lib.cpp'
 })
 platform_specifics()
-
-links { 'luabind' , settings.links[OS] }
+links { 'luabind' }
+boost:set_links()
+lua:set_links()
 
 make_console_app('test_blufs',{ 'test.cpp' })
-links {'luabind','blufs', settings.links[OS]}
+links {'luabind', 'blufs' }
+boost:set_links()
+lua:set_links()
 platform_specifics()
 run_target_after_build()
